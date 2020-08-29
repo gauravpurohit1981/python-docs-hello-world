1a336ade864e0acebe5a848723460c582ceb934a
gauravpurohit1981 <43321789+gauravpurohit1981@users.noreply.github.com>
2020-08-29 23:28:41 +0100

    Update manage.py

--- manage.py
+++ manage.py
@@ -360,4 +360,4 @@ parser.add_argument('username')
 parser.add_argument('password')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=8000)
